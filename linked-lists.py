7089e88e850a468d18fe9da574e8c5351885f187
Jessica Langston <jessicalangston@Jessicas-MacBook-Pro-2.local>
2015-09-14 17:41:47 -0700

    Initial commit.

---
+++ linked-lists.py
@@ -0,0 +1,4 @@
+# takes in: linked list
+# returns: head of a new reversed linked list
+
+def reverse_ll(ll):
---
+++ lists.py
@@ -0,0 +1,4 @@
+# practice question from talk by parker from interview cake
+
+def get_greatest_diff(list_nums):
+    
